ac07ab1aa9cb4bb6cdfe72b54cd27ecc971a1c58
nguyenquangos0302work <nguyenquangos0302work@gmail.com>
2024-12-17 09:24:32 +0700

    Update 175_Combine_Two_Tables.sql

--- practice-sql/175_Combine_Two_Tables.sql
+++ practice-sql/175_Combine_Two_Tables.sql
@@ -2,26 +2,18 @@ DROP SCHEMA IF EXISTS leetcode CASCADE;
 CREATE SCHEMA leetcode;
 SET search_path TO leetcode, public;
 
-DROP TABLE IF EXISTS Person;
 CREATE TABLE Person (
   personId INT PRIMARY KEY,
   lastName VARCHAR(50),
 	firstName VARCHAR(50)
 );
 
-DROP TABLE IF EXISTS Address;
 CREATE TABLE Address (
 	addressId INT PRIMARY KEY,
 	personId INT,
 	city VARCHAR(100),
 	state VARCHAR(100),
 	FOREIGN KEY (personId) REFERENCES Person(personId)
-);
-
-DROP TABLE IF EXISTS Employee;
-CREATE TABLE Employee (
-    id INT PRIMARY KEY,
-    salary INT
 );
 
 -- Insert data into Person table
